b29d75e0bc8485fc43eea8ba88e1bccdf96b5011
torreswoo <torreswoo@gmail.com>
2018-03-14 13:46:32 +0900

    config | add configs & Logging (github.com/go-kit/kit/log)

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -2,7 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/torreswoo/hello/configs"
+	"github.com/go-kit/kit/log"
+	"os"
 )
 func main(){
 	fmt.Println("Hello world");
+
+	// ENV - get config/ Flag
+	flag := configs.GetFlag()
+
+	// ENV - setting Logger
+	var logger log.Logger
+	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
+	logger.Log(
+		"version", flag.Version,
+		"gitHash", flag.GitHash,
+		"buildTime", flag.BuildTime,
+		"started", flag.Started,
+	)
+
+	logger.Log("message", "START APPLICATION")
 }
