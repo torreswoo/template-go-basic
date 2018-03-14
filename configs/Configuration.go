dc4b409dfe32b3ac0d5edddd066a101b38b15fa3
torreswoo <torreswoo@gmail.com>
2018-03-14 13:46:03 +0900

    config | add configs/Configuration.go for -ldflags & ENV

---
+++ configs/Configuration.go
@@ -0,0 +1,28 @@
+package configs
+
+import(
+	"time"
+)
+
+var (
+	Version = "undefined"
+	BuildTime = "undefined"
+	GitHash = "undefined"
+	Started = time.Now().UTC().Format(time.RFC3339)
+)
+
+type Flag struct {
+	Version string
+	BuildTime string
+	GitHash string
+	Started string
+}
+
+func GetFlag() Flag{
+	return Flag{
+		Version:Version,
+		BuildTime:BuildTime,
+		GitHash:GitHash,
+		Started:Started,
+	}
+}
